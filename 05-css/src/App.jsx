5f1f1ef38db3a245a6d28822d0af208cf1545b97
devikapandya1 <devikajoshi2923@gmail.com>
2026-01-02 12:23:16 +0530

    learning react

---
+++ 05-css/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './component/button/Button'
+import Header from './component/header/Header'
+
+const App = () => {
+  return (
+    <div>
+      <Button />
+      <Header/>
+    </div>
+  )
+}
+
+export default App
---
+++ 05-css/src/component/button/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Button.module.css'
+const Button = () => {
+  return (
+    <div  className={styles.btn}>
+      <h1>Button</h1>
+    </div>
+  )
+}
+
+export default Button
